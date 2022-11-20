26f9d6568d2f1ca40bdbb13b58532fc1d328e36c
ImmoralComedian <liciys0@gmail.com>
2022-11-20 21:13:49 +0300

    Поменял + на - в файле main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <std.h>
+
+int main() {
+	int a, b;
+	printf("Enter A and B from keyboard\n");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("A-B=%d",a-b)
+	return 0;
+}
